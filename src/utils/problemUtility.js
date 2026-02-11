b42715be3bd5938da3ca27b338efc855f7c1f082
Aditya jaiwal <adityajaiswal0923@gmail.com>
2026-02-11 15:31:58 +0530

    created submit batch option

--- src/utils/problemUtility.js
+++ src/utils/problemUtility.js
@@ -10,6 +10,33 @@ const getLanguageById=(lang)=>{
 }
 
 const submitBatch=async(submissions)=>{
+    const options = {
+  method: 'POST',
+  url: 'https://judge0-ce.p.rapidapi.com/submissions/batch',
+  params: {
+    base64_encoded: 'true'
+  },
+  headers: {
+    'x-rapidapi-key': 'ab99c6ec42mshfd636ec7c6687efp1b9043jsna684835b0591',
+    'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
+    'Content-Type': 'application/json'
+  },
+  data: {
+    submissions
+  }
+};
+
+async function fetchData() {
+	try {
+		const response = await axios.request(options);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+ return await fetchData();
+
 
 
 
