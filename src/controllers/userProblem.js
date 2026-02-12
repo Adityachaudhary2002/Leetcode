1f4a2a1d224437bdae3a8340b6453c2f66bca8be
Aditya jaiwal <adityajaiswal0923@gmail.com>
2026-02-12 14:25:51 +0530

    import and export the module

--- src/controllers/userProblem.js
+++ src/controllers/userProblem.js
@@ -1,5 +1,6 @@
 
-const {getLanguageById,submitbatch}= require("../utils/problemUtility");
+const {getLanguageById,submitbatch,submitToken}= require("../utils/problemUtility");
+const Problem= require("../models/problem")
  const createProblem=async(req,res)=>{
     const{title,description,difficulty,tags,visibleTestCases,hiddenTestCases,startcode,referenceSolution,ProblemCreator}=req.body;
 
@@ -13,19 +14,32 @@ const {getLanguageById,submitbatch}= require("../utils/problemUtility");
             //expectedOutput:
             const languageId=getLanguageById(language);
             // I am creating Batch submission
-            const submissions=visibleTestCases.map((Input,output)=>({
+            const submissions=visibleTestCases.map((testcase)=>({
                 source_code:completeCode,
                 language_id:languageId,
-                stdin:Input,
-                expected_output:output,
+                stdin:testcase.Input,
+                expected_output:testcase.output,
             }));
             const submitResult=await submitbatch(submissions);
+            const resultToken=submitResult.map((value)=>value.token);
 
+            const testResult=await submitToken(resultToken);
+            for(const test of testResult){
+                if(test.status_id!=3){
+                    return res.status(400).send("Error Occured");
+                }
+            }
         }
 
-        
+        // we can store it in our DB
+         const userproblem=await ProblemCreator.create({
+            ...req.body,
+            problemCreator: req.result._id
+        });
+        res.status(201).send("Problem saved Successfully");
     }
     catch(err){
-
+     res.status(400).send("Error"+err);
     }
 }
+module.exports=createProblem;
--- src/routes/problemCreator.js
+++ src/routes/problemCreator.js
@@ -1,13 +1,16 @@
 const express=require('express');
 const problemCreator=express.Router();
 const adminMiddleware=require("../middleware/adminMiddleware");
+const createProblem=require("../controllers/userProblem");
 
 
 //create
 problemRouter.post("/create",adminMiddleware,createProblem);
-problemRouter.patch("/:id",updateProblem);
-problemRouter.delete("/:id",deleteProblem);
+// problemRouter.patch("/:id",updateProblem);
+// problemRouter.delete("/:id",deleteProblem);
 
-problemRouter.get("/:id",getProblemById);
-problemRouter.get("/",getAllProblem);
-problemRouter.get("/user",solvedAllProblembyuser);
+// problemRouter.get("/:id",getProblemById);
+// problemRouter.get("/",getAllProblem);
+// problemRouter.get("/user",solvedAllProblembyuser);
+
+module.exports=problemRouter;
