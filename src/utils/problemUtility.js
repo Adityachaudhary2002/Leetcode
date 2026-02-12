7a8e4aa075618795148f9cf0991c56d1feb5fb2b
Aditya jaiwal <adityajaiswal0923@gmail.com>
2026-02-12 14:22:58 +0530

    create submit Token

--- src/utils/problemUtility.js
+++ src/utils/problemUtility.js
@@ -41,4 +41,53 @@ async function fetchData() {
 
 
 }
-module.export=(getLanguageById,submitBatch);
+
+
+const waiting =async(timer)=>{
+  setTimeout(()=>{
+    return 1;
+  },timer);
+}
+
+const submitToken=async(resultTokens)=>{
+  const options = {
+    method: 'GET',
+    url: 'https://judge0-ce.p.rapidapi.com/submissions/batch',
+    params: {
+      tokens: resultToken.join(","),
+      base64_encoded: 'false',
+      fields: '*'
+    },
+    headers: {
+      'x-rapidapi-key': 'ab99c6ec42mshfd636ec7c6687efp1b9043jsna684835b0591',
+      'x-rapidapi-host': 'judge0-ce.p.rapidapi.com'
+    }
+  };
+  
+  async function fetchData() {
+    try {
+      const response = await axios.request(options);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  
+   while(true){
+  
+   const result =  await fetchData();
+  
+    const IsResultObtained =  result.submissions.every((r)=>r.status_id>2);
+  
+    if(IsResultObtained)
+      return result.submissions;
+  
+    
+    await waiting(1000);
+  }
+  
+  
+ 
+}
+module.export={getLanguageById,submitBatch,submitToken};
