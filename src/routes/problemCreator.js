873077ba2fba44a9acf1446b758e9b0635d56a65
Aditya jaiwal <adityajaiswal0923@gmail.com>
2026-02-14 22:07:11 +0530

    created crud operations in userproblem and import in problemcreator

--- src/routes/problemCreator.js
+++ src/routes/problemCreator.js
@@ -1,16 +1,16 @@
 const express=require('express');
 const problemCreator=express.Router();
 const adminMiddleware=require("../middleware/adminMiddleware");
-const createProblem=require("../controllers/userProblem");
-
+const {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem}=require("../controllers/userProblem").default;
+const userMiddleware=require("../middleware/userMiddleware");
 
 //create
 problemRouter.post("/create",adminMiddleware,createProblem);
-// problemRouter.patch("/:id",updateProblem);
-// problemRouter.delete("/:id",deleteProblem);
+ problemRouter.put("/update/:id",adminMiddleware,updateProblem);
+problemRouter.delete("/delete/:id",adminMiddleware,deleteProblem);
 
-// problemRouter.get("/:id",getProblemById);
-// problemRouter.get("/",getAllProblem);
-// problemRouter.get("/user",solvedAllProblembyuser);
+ problemRouter.get("problemById/:id",userMiddleware,getProblemById);
+ problemRouter.get("/getAllProblem/",userMiddleware,getAllProblem);
+ problemRouter.get("/problemSolverByUser",userMiddleware,solvedAllProblembyuser);
 
 module.exports=problemRouter;
