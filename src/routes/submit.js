08f536ce02ad1ba413007ff559e4a57404c07096
Aditya jaiwal <adityajaiswal0923@gmail.com>
2026-02-16 21:42:17 +0530

    created a submit file

---
+++ src/routes/submit.js
@@ -0,0 +1,6 @@
+const express=require('express');
+const submitRouter=express.Router();
+const userMiddleware=require('../middleware/userMiddleware')
+const submitCode=require("../controllers/usersubmission")
+
+submitRouter.post("/submit/:id",userMiddleware, submitCode);
