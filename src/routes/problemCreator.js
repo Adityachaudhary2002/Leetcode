247e9d2e4040cf9b324454ed0cc8a604a7a2d4b0
Aditya jaiwal <adityajaiswal0923@gmail.com>
2026-02-26 23:41:11 +0530

    commit

--- src/routes/problemCreator.js
+++ src/routes/problemCreator.js
@@ -1,16 +1,17 @@
 const express=require('express');
 const problemCreator=express.Router();
 const adminMiddleware=require("../middleware/adminMiddleware");
-const {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem,solvedAllProblemByUser}=require("../controllers/userProblem").default;
+const {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem,solvedAllProblemByUser,submittedProblem}=require("../controllers/userProblem").default;
 const userMiddleware=require("../middleware/userMiddleware");
 
 //create
 problemRouter.post("/create",adminMiddleware,createProblem);
- problemRouter.put("/update/:id",adminMiddleware,updateProblem);
+problemRouter.put("/update/:id",adminMiddleware,updateProblem);
 problemRouter.delete("/delete/:id",adminMiddleware,deleteProblem);
 
- problemRouter.get("problemById/:id",userMiddleware,getProblemById);
- problemRouter.get("/getAllProblem/",userMiddleware,getAllProblem);
- problemRouter.get("/problemSolvedByUser",userMiddleware,solvedAllProblemByUser);
+problemRouter.get("problemById/:id",userMiddleware,getProblemById);
+problemRouter.get("/getAllProblem/",userMiddleware,getAllProblem);
+problemRouter.get("/problemSolvedByUser",userMiddleware,solvedAllProblemByUser);
+problemRouter.ger("/submittedProblem",userMiddleware,submittedProblem);
 
 module.exports=problemRouter;
