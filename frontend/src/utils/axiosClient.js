8c673690bc5478ae65c3f97035e415cc4153516c
Aditya jaiwal <adityajaiswal0923@gmail.com>
2026-05-16 13:32:14 +0530

    all

---
+++ frontend/src/utils/axiosClient.js
@@ -0,0 +1,10 @@
+import axios from"axios"
+
+const axiosClient=axios.create({
+    baseURL:'http://localhost:3000',
+    withCredentials:true,
+    headers:{
+        'content-Type':'application/json'
+    }
+});
+export default axiosClient;
